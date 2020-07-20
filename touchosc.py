c3f3ea66c2862e6afcd4fc53c4b3d7734f37e6ef
Martin Wittmann <me@martinwittmann.at>
2020-07-20 08:48:13 +0200

    Initial commit. First tests of jinja templating, getting started with json as data format for defining components.

---
+++ touchosc.py
@@ -0,0 +1,135 @@
+#!/usr/bin/python
+# Compile dynamic templates to touch osc files.
+import sys, os, base64, shutil, json
+from jinja2 import Environment, FileSystemLoader
+
+def s(str):
+  return base64.b64encode(str)
+
+
+
+
+
+if (len(sys.argv) < 2):
+  print("Usage: ./touchosc.py [components-file]\nSee example.json.")
+  sys.exit(1)
+
+components_file = sys.argv[1]
+output_name = os.path.splittext(components_file)
+
+template_dir = 'templates/'
+output_dir = 'output/' + output_name
+
+# We always start with layout.xml, this is whatthe touchosc file format expects.
+template_name = 'layout.xml'
+template_file = os.path.abspath(template_dir + template_name + '.xml')
+
+# Create the output dir for this components file if not existing.
+if (not os.path.isdir(output_dir)):
+  os.mkdir(output_dir)
+
+# Always using index.html since the is the only filename touchosc uses.
+output_file = os.path.abspath(output_dir + '/index.xml')
+
+if (not os.path.isfile(template_file)):
+  print("Template file " + template_file + " does not exist.")
+  sys.exit(1)
+
+
+  
+env = Environment(
+  loader=FileSystemLoader(os.path.abspath(template_dir)),
+)
+
+template = env.get_template(template_name + '.xml')
+
+layout = {
+  'width': 2000,
+  'height': 1200,
+  'column_spacer': 20,
+  'right': {
+    'width': 400,
+    'spacer': 10,
+    'button_height': 130,
+    'button_height_sm': 70,
+    'button_width': 400,
+    'button_half_width': 195,
+    'tempo_y': 601,
+  },
+}
+layout['right']['x'] = layout['width'] - layout['right']['width'] - layout['column_spacer']
+
+data = {
+  'label_1': s('Gallo Wuttu'),
+  'toggle_click': {
+    'text': s('CLICK MUTE'),
+  },
+  'buttons': {
+    'decrease_tempo': {
+      'name': s('decrease_tempo'),
+      'width': layout['right']['button_half_width'],
+      'height': layout['right']['button_height_sm'],
+      'x': layout['right']['x'],
+      'y': layout['right']['tempo_y'],
+      'color': 'orange',
+      'osc': s('/a/41130'),
+      'type': 'push',
+      'local_off': 'false',
+      'sp': 'true',
+      'sr': 'false',
+    },
+    'decrease_tempo_label': {
+      'name': s('decrease_tempo_label'),
+      'text': s('-'),
+      'y': layout['right']['tempo_y'] - 5,
+      'type': 'labelh',
+      'background': 'false',
+      'outline': 'false',
+      'color': 'gray',
+      'size': '75',
+    },
+    'increase_tempo': {
+      'name': s('increase_tempo'),
+      'width': layout['right']['button_half_width'],
+      'height': layout['right']['button_height_sm'],
+      'x': layout['right']['x'] + layout['right']['button_half_width'] + layout['right']['spacer'],
+      'y': layout['right']['tempo_y'],
+      'color': 'orange',
+      'osc': s('/a/41129'),
+      'type': 'push',
+      'local_off': 'false',
+      'sp': 'true',
+      'sr': 'false',
+    },
+    'increase_tempo_label': {
+      'name': s('increase_tempo_label'),
+      'text': s('+'),
+      'type': 'labelh',
+      'background': 'false',
+      'outline': 'false',
+      'color': 'gray',
+      'size': '65',
+    },
+    'record': {
+      'name': s('record'),
+      'width': layout['right']['button_width'],
+      'height': layout['right']['button_height'],
+      'x': layout['right']['x'],
+      'y': 1014,
+      'color': 'red',
+      'osc': s('/record'),
+      'type': 'push',
+      'local_off': 'false',
+      'sp': 'true',
+      'sr': 'false',
+    },
+  },
+}
+output = template.render(**data)
+file_handle = open(output_file, 'w+')
+file_handle.write(output)
+file_handle.close()
+
+
+shutil.make_archive(output_dir + '.touchosc', 'zip', output_dir + template_name)
+os.rename(output_dir + ".touchosc.zip", output_dir + ".touchosc")
